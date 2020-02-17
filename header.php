92f282326d023bb014bdcf94a2b391d05dc024cc
AnnaNordberg <56160770+AnnaNordberg@users.noreply.github.com>
2020-02-17 09:48:00 +0100

    add boilerplate

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>
+<?php
+    if(empty($title))
+        echo "Min sida!";
+
+    else
+        echo $title;
+?>
+
+</title>
+
+ <link rel="stylesheet" href="css/main.css" type="text/css" />
+ <script src="js/main.js"></script>
+</head>
+<body>
+<h1>Hej hej</h1>
+<a href="index.php">Start</a> | <a href="about.php">Om oss</a>
+<hr />
+    
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include("header.php");
+
+?>
+
+content
+
+
+<?php
+
+include("footer.php");
+
+?>
+
+</body>
+</html>
